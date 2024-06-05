6738255392adcf259319dba02ee670d0b113ef34
CodeCraftRisan <152704503+CodeCraftRisan@users.noreply.github.com>
2024-06-05 07:13:01 +0600

    login page updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,3 +18,70 @@ const Login = () => {
    toast.error("Passwords do not match");
    return;
   }
+setTimeout(() => {
+       toast.success("Login successful!");
+       setIsAuthenticated(true);
+       navigateTo("/");
+       setEmail("");
+       setPassword("");
+       setConfirmPassword("");
+      }, 1000);
+     };
+    
+     if (isAuthenticated) {
+      return <Navigate to={"/"} />;
+     }
+    
+     return (
+      <>
+       <div className="container form-component login-form">
+        <h2>Sign In</h2>
+        <p>Please Login To Continue</p>
+        <p>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat culpa
+         voluptas expedita itaque ex, totam ad quod error?
+        </p>
+        <form onSubmit={handleLogin}>
+         <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+         />
+         <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+         />
+         <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+         />
+         <div
+          style={{
+           gap: "10px",
+           justifyContent: "flex-end",
+           flexDirection: "row",
+          }}
+         >
+          <p style={{ marginBottom: 0 }}>Not Registered?</p>
+          <Link
+           to={"/register"}
+           style={{ textDecoration: "none", color: "#271776ca" }}
+          >
+           Register Now
+          </Link>
+         </div>
+         <div style={{ justifyContent: "center", alignItems: "center" }}>
+          <button type="submit">Login</button>
+         </div>
+        </form>
+       </div>
+      </>
+     );
+    };
+    
+    export default Login;
